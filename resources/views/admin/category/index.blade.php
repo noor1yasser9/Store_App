85ec8cb8c6f68a8a3d3ecb7747223c85fc7167f8
OMEN <mrh61037@gmail.com>
2021-11-19 21:53:02 +0200

    first commit

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,138 @@
+@extends('admin.layouts.master_layout')
+@section('css')
+<!-- Internal Data table css -->
+<link href="{{URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
+
+@endsection
+@section('page-header')
+@endsection
+@section('content')
+				<!-- row opened -->
+				<div class="mt-5 row row-sm">
+					<div class="col-xl-12">
+						<div class="card">
+							<div class="card-header pb-0">
+								<div class="d-flex justify-content-between">
+									<h4 class="card-title mg-b-0">Categories</h4>
+                                    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#add">
+                                        Add Category &ThickSpace; &ThickSpace;
+                                        <i class="fa fa-plus"></i>
+                                     </button>
+								</div>
+							</div>
+							<div class="card-body">
+								<div class="table-responsive">
+									<table class="table text-md-nowrap" id="example1">
+										<thead>
+											<tr>
+												<th class="border-bottom-0">#</th>
+												<th class="border-bottom-0">Name</th>
+                                                <th class="border-bottom-0"><i class="fa fa-cogs"></i></th>
+											</tr>
+										</thead>
+										<tbody>
+										@foreach ( $categories as  $category)
+                                        <tr>
+                                            <td>{{$loop->index+1}}</td>
+                                            <td>{{$category->name}}</td>
+                                            <td>
+                                                <form action="{{ route('category.destroy', $category->id) }}" style="display: inline;"
+                                                     method="POST">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button class="btn btn-danger remove">
+                                                        <i class="fa fa-trash"></i>
+                                                </button>
+                                                </form>
+                                                <button class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#edit{{$category->id}}">
+                                                    <i class="fa fa-edit"></i></button>
+                                            </td>
+                                            <div class="modal fade" id="edit{{$category->id}}" tabindex="-1" aria-labelledby="edit" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                  <div class="modal-content">
+                                                    <div class="modal-header">
+                                                      <h5 class="modal-title" id="exampleModalLabel">Add Category</h5>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <div class="card-body pt-0">
+                                                            <form class="form-horizontal"  method="POST" action="{{ route('category.update', $category->id) }}">
+                                                                @csrf
+                                                                @method('PUT')
+                                                                <div class="form-group">
+                                                                    <input type="text" class="form-control" name="name" value="{{$category->name}}" id="inputName" placeholder="Name Category">
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                    <button type="submit" class="btn btn-primary">Save</button>
+                                                                   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+
+                                        </tr>
+                                        @endforeach
+										</tbody>
+									</table>
+                                    <div class="modal fade" id="add" tabindex="-1" aria-labelledby="add" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                          <div class="modal-content">
+                                            <div class="modal-header">
+                                              <h5 class="modal-title" id="exampleModalLabel">Add Category</h5>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="card-body pt-0">
+                                                    <form class="form-horizontal"  method="POST" action="{{ route('category.store') }}">
+                                                        @csrf
+                                                        <div class="form-group">
+                                                            <input type="text" class="form-control" name="name" id="inputName" placeholder="Name Category">
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="submit" class="btn btn-primary">Save</button>
+                                                           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+
+                                          </div>
+                                        </div>
+                                      </div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+@endsection
+
+
+@section('js')
+<!-- Internal Data tables -->
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jszip.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+<!--Internal  Datatable js -->
+<script src="{{URL::asset('assets/js/table-data.js')}}"></script>
+
+{{-- <script src="sweetalert2.min.js"></script> --}}
+
+@endsection
---
+++ resources/views/admin/layouts/master_layout.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="Description" content="Bootstrap Responsive Admin Web Dashboard HTML5 Template">
+		<meta name="Author" content="Spruko Technologies Private Limited">
+		<meta name="Keywords" content="admin,admin dashboard,admin dashboard template,admin panel template,admin template,admin theme,bootstrap 4 admin template,bootstrap 4 dashboard,bootstrap admin,bootstrap admin dashboard,bootstrap admin panel,bootstrap admin template,bootstrap admin theme,bootstrap dashboard,bootstrap form template,bootstrap panel,bootstrap ui kit,dashboard bootstrap 4,dashboard design,dashboard html,dashboard template,dashboard ui kit,envato templates,flat ui,html,html and css templates,html dashboard template,html5,jquery html,premium,premium quality,sidebar bootstrap 4,template admin bootstrap 4"/>
+		@include('admin.layouts.head')
+        @toastr_css
+        {{-- {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> --}}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    	</head>
+
+	<body class="main-body app sidebar-mini">
+		<!-- Loader -->
+		<div id="global-loader">
+			<img src="{{URL::asset('assets/img/loader.svg')}}" class="loader-img" alt="Loader">
+		</div>
+		<!-- /Loader -->
+		@include('admin.layouts.main-sidebar')
+		<!-- main-content -->
+		<div class="main-content app-content">
+			@include('admin.layouts.main-header')
+			<!-- container -->
+			<div class="container-fluid">
+				@yield('page-header')
+
+                @if ($errors->any())
+                <div class=" mt-5 alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                           {{ $error }}<br>
+                        @endforeach
+
+                </div>
+                @endif
+
+				@yield('content')
+				@include('admin.layouts.sidebar')
+				@include('admin.layouts.models')
+            	@include('admin.layouts.footer')
+				@include('admin.layouts.footer-scripts')
+
+                @jquery
+                @toastr_js
+                @toastr_render
+	</body>
+</html>
